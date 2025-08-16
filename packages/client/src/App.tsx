612207f41767cb0225a82a137637989b732d573f
Raghav123-30 <raghavendrabhat2018@gmail.com>
2025-08-16 16:09:18 +0530

    Initialize shadcn and install components

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,3 +1,11 @@
+import { Button } from "./components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "./components/ui/card";
 import { trpc } from "./lib/trpc";
 
 export default function App() {
@@ -12,5 +20,20 @@ export default function App() {
     return <p className="text-red-500 text-sm">Something went wrong</p>;
   }
 
-  return <p className="text-green-500 text-sm">{data?.message}</p>;
+  return (
+    <div className="grid min-h-screen">
+      <Card className="self-center max-w-md w-full mx-auto">
+        <CardHeader>
+          <CardTitle>Collaborative Task Board</CardTitle>
+          <CardDescription>
+            App to practice react query and tRPC
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="space-y-3">
+          <p className="text-muted-foreground">{data?.message}</p>
+          <Button>Lets get started</Button>
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
